926bfea939a8f661726519f7420196512a9d0cef
WandersoNoleto <onlywanderson@gmail.com>
2022-10-13 10:46:17 -0300

    config django-admin

--- users/models.py
+++ users/models.py
@@ -1,41 +1,70 @@
-from email.policy import default
-from statistics import mode
-from tabnanny import verbose
-
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin)
 from django.db import models
 
 
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        user = self.model(
+            email = self.normalize_email(email)
+        )
+
+        user.is_active    = True
+        user.is_staff     = False
+        user.is_superuser = False 
+
+        if password:
+            user.set_password(password)
+
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        superuser = self.create_user(
+            email = self.normalize_email(email),
+            password = password
+        )
+
+        superuser.is_active    = True
+        superuser.is_staff     = True
+        superuser.is_superuser = True 
+
+        superuser.set_password(password)
+        superuser.save()
+        return superuser
+
+
 class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(
-        verbose_name="E-mail do usuário",
-        max_length=194,
-        unique=True
+        verbose_name = "E-mail do usuário",
+        max_length = 194,
+        unique = True
     )
 
     is_active = models.BooleanField(
-        verbose_name ="Usuário está ativo",
-        default=True
+        verbose_name = "Usuário está ativo",
+        default = True
     )
 
     is_staff = models.BooleanField(
-        verbose_name="Usuário é da equipe de desenvolvimento",
-        default=False
+        verbose_name = "Usuário é da equipe de desenvolvimento",
+        default = False
     )
 
     is_superuser = models.BooleanField(
-        verbose_NAME="Usuário é um superusuario",
-        default=False
+        verbose_name = "Usuário é um superusuario",
+        default = False
     )
 
     USERNAME_FIELD = "email"
+    objects = UserManager()
 
     class Meta:
-        verbose_name= "Usuário"
+        verbose_name        = "Usuário"
         verbose_name_plural = "Usuários"
-        db_table = "usuário"
+        db_table            = "usuario"
 
     def __str__(self) -> str:
         return self.email
