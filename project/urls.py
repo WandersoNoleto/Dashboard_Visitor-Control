5492d4a94574a97dea176abb2ae15b8acd077998
WandersoNoleto <onlywanderson@gmail.com>
2022-10-15 15:28:06 -0300

    Visitor most important functionalities finished

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from users.views import index
-from visitors.views import visitor_register
+from visitors.views import visit_completed, visitor_info, visitor_register
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name = "Home"),
-    path ("registrar-visitante/", visitor_register, name="registrar_visitante")
+    path ("registrar-visitante/", visitor_register, name="visitor_register"),
+    path("visitantes/<int:id>/", visitor_info, name="visitor_info"),
+    path("visitantes/<int:id>/finalizar/", visit_completed, name="visit_completed")
 ]
